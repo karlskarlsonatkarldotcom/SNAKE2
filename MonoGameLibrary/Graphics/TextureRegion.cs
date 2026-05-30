27f1da4e593707950a9bbd3acb87bfbbafe897a8
Karl <karlskarlsonatkarldotcom@proton.me>
2026-05-30 08:33:50 -0700

    up to chapter 21

--- MonoGameLibrary/Graphics/TextureRegion.cs
+++ MonoGameLibrary/Graphics/TextureRegion.cs
@@ -34,6 +34,27 @@ public class TextureRegion
     public TextureRegion() { }
 
     /// <summary>
+    /// Gets the top normalized texture coordinate of this region.
+    /// </summary>
+    public float TopTextureCoordinate => SourceRectangle.Top / (float)Texture.Height;
+
+    /// <summary>
+    /// Gets the bottom normalized texture coordinate of this region.
+    /// </summary>
+    public float BottomTextureCoordinate => SourceRectangle.Bottom / (float)Texture.Height;
+
+    /// <summary>
+    ///  Gets the left normalized texture coordinate of this region.
+    /// </summary>
+    public float LeftTextureCoordinate => SourceRectangle.Left / (float)Texture.Width;
+
+    /// <summary>
+    /// Gets the right normalized texture coordinate of this region.
+    /// </summary>
+    public float RightTextureCoordinate => SourceRectangle.Right / (float)Texture.Width;
+
+
+    /// <summary>
     /// Creates a new texture region using the specified source texture.
     /// </summary>
     /// <param name="texture">The texture to use as the source texture for this texture region.</param>
--- Snake2/Game1.cs
+++ Snake2/Game1.cs
@@ -1,18 +1,16 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+﻿using DungeonSlime.Scenes;
+using Gum.Forms;
+using Gum.Forms.Controls;
 using MonoGameLibrary;
-using MonoGameLibrary.Graphics;
+using MonoGameGum;
+using Microsoft.Xna.Framework.Media;
 
 namespace Snake2;
 
 public class Game1 : Core
 {
-    // Defines the slime animated sprite.
-    private AnimatedSprite _slime;
-
-    // Defines the bat animated sprite.
-    private AnimatedSprite _bat;
+    // The background theme song.
+    private Song _themeSong;
 
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
@@ -21,56 +19,57 @@ public class Game1 : Core
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
+
+        // Start playing the background music
+        Audio.PlaySong(_themeSong);
+
+        // Initialize the Gum UI service
+        InitializeGum();
+
+        // Start the game with the title scene.
+        ChangeScene(new TitleScene());
     }
 
     protected override void LoadContent()
     {
-        // Create the texture atlas from the XML configuration file.
-        TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
-
-        // Create the slime animated sprite from the atlas.
-        _slime = atlas.CreateAnimatedSprite("slime-animation");
-        _slime.Scale = new Vector2(4.0f, 4.0f);
-
-        // Create the bat animated sprite from the atlas.
-        _bat = atlas.CreateAnimatedSprite("bat-animation");
-        _bat.Scale = new Vector2(4.0f, 4.0f);
+        // Load the background theme music.
+        _themeSong = Content.Load<Song>("audio/theme");
     }
 
-    protected override void Update(GameTime gameTime)
+    private void InitializeGum()
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        // Initialize the Gum service. The second parameter specifies
+        // the version of the default visuals to use. V3 is the latest
+        // version.
+        GumService.Default.Initialize(this, DefaultVisualsVersion.V3);
 
-        // Update the slime animated sprite.
-        _slime.Update(gameTime);
+        // Tell the Gum service which content manager to use. We will tell it to
+        // use the global content manager from our Core.
+        GumService.Default.ContentLoader.XnaContentManager = Core.Content;
 
-        // Update the bat animated sprite.
-        _bat.Update(gameTime);
+        // Register keyboard input for UI control.
+        FrameworkElement.KeyboardsForUiControl.Add(GumService.Default.Keyboard);
 
-        base.Update(gameTime);
+        // Register gamepad input for Ui control.
+        FrameworkElement.GamePadsForUiControl.AddRange(GumService.Default.Gamepads);
+
+        // Customize the tab reverse UI navigation to also trigger when the keyboard
+        // Up arrow key is pushed.
+        FrameworkElement.TabReverseKeyCombos.Add(
+           new KeyCombo() { PushedKey = Microsoft.Xna.Framework.Input.Keys.Up });
+
+        // Customize the tab UI navigation to also trigger when the keyboard
+        // Down arrow key is pushed.
+        FrameworkElement.TabKeyCombos.Add(
+           new KeyCombo() { PushedKey = Microsoft.Xna.Framework.Input.Keys.Down });
+
+        // The assets created for the UI were done so at 1/4th the size to keep the size of the
+        // texture atlas small.  So we will set the default canvas size to be 1/4th the size of
+        // the game's resolution then tell gum to zoom in by a factor of 4.
+        GumService.Default.CanvasWidth = GraphicsDevice.PresentationParameters.BackBufferWidth / 4.0f;
+        GumService.Default.CanvasHeight = GraphicsDevice.PresentationParameters.BackBufferHeight / 4.0f;
+        GumService.Default.Renderer.Camera.Zoom = 4.0f;
     }
 
-    protected override void Draw(GameTime gameTime)
-    {
-        // Clear the back buffer.
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // Begin the sprite batch to prepare for rendering.
-        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
-
-        // Draw the slime sprite.
-        _slime.Draw(SpriteBatch, Vector2.One);
-
-        // Draw the bat sprite 10px to the right of the slime.
-        _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
-
-        // Always end the sprite batch when finished.
-        SpriteBatch.End();
-
-        base.Draw(gameTime);
-    }
 }
